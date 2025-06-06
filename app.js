f032b588004171297965618926657d46864e3788
Triliam <taisbuenovidotto@gmail.com>
2025-06-06 11:45:40 -0300

    alteraçoes permissoes na nuvem, alteraçoes no back app.ja e instalaçoes de dependencias, integraçao com o front

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const cors = require('cors'); 
+
 const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
+const authRouter = require('express').Router(); 
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
 const ticketsRouter = require('./routes/tickets');
 
 const app = express();
 
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:8081' 
+}));
+
+app.use(express.json()); 
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
