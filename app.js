913e15ea04ec2ce0b2df4ddb952bed6d9f99adca
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2025-06-08 07:05:55 -0300

    fix: fix router for "/auth" and enable cors for all routes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
-const authRouter = require('express').Router(); 
+const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
 const ticketsRouter = require('./routes/tickets');
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:8081' 
-}));
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
